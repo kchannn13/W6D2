b5bfab4b04d4e8041176360dd5cca452977792ee
Kevin Chan <chankevin13@gmail.com>
2023-02-14 16:08:11 -0500

    finished 00

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,14 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
+    names.each do |n|
+      define_method(n) do
+        instance_variable_get("@" + n.to_s)
+      end
+
+      define_method(n.to_s + "=") do |i|
+        instance_variable_set("@" + n.to_s, i) 
+      end
+    end
     # ...
   end
 end
